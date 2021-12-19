596683d0fe52d49b69b1ef5f4c913ab830a7e5ab
knnorbi <kluka.norbert@gmail.com>
2021-12-19 16:42:52 +0100

    Initial commit

---
+++ LINQ_2/Dolgozo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_2
+{
+    class Dolgozo
+    {
+        public string Nev { get; private set; }
+        public bool Ferfi { get; private set; }
+        public string Reszleg { get; private set; }
+        public int BelepesEve { get; private set; }
+        public int Ber { get; private set; }
+
+        public Dolgozo(string sor)
+        {
+            string[] adatok = sor.Split(';');
+
+            Nev = adatok[0];
+            Ferfi = adatok[1] == "férfi";
+            Reszleg = adatok[2];
+            BelepesEve = int.Parse(adatok[3]);
+            Ber = int.Parse(adatok[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} ({Reszleg})";
+        }
+
+        public static Dolgozo[] Betolt(string path)
+        {
+            return File.ReadAllLines(path).Skip(1).Select(d => new Dolgozo(d)).ToArray();
+        }
+    }
+}
---
+++ LINQ_2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolgozo[] dolgozok = Dolgozo.Betolt("bérek.txt");
+            /*
+             * Az alábbi feladatban egy cég dolgozói adatait kell elemezned.
+             * A dolgozó már be vannak töltve a 'dolgozok' tömbbe.
+             * Oldd meg a feladataokat LINQ-t felhasználval, ha lehetséges!
+             * Az eredményeket írd ki a konzolra!
+             */
+
+            // 1. feladat: Mennyi dolgozója van a cégnek?
+
+            // 2. feladat: Hányan dolgoznak a beszerzés részlegen?
+
+            // 3. feladat: Mennyi az átlag kereset a cégnél?
+
+            // 4. feladat: Havonta mennyi munkabért kell utalni a cégnek a dolgozóknak?
+
+            // 5. feladat: Mennyi a legmagasabb fizetés a cégnél?
+
+            // 6. feladat: Mennyi a legalacsonyabb fizetés a cégnél?
+
+            // 7. feladat: A dolgozók hány százaléka nő?
+
+            // 8. feladat: Csatlakozott-e 2020-ban munkavállaló a céghez?
+
+            // 9. feladat: Keres-e legalább százezer forintot minden dolgozó?
+
+            // 10. feladat: Ki dolgozik a legrégebben a pénzügyi részlegen? (Feltételezve, hogy egy van.)
+            
+            // 11. feladat: Kitünetésben részesülnek a 20 éve belépett dolgozók. Kik ők?
+
+            // 12. feladat: Kik keresnek átlag felett?
+
+            // 13. feladat: Mennyi átlag fizetés a beszerzési részlegen?
+
+            // 14. feladat: Csatlakozott-e lakatos a céghez 2000-ben vagy után?
+
+            // 15. feladat: Mennyi dolgozó keresi a legalacsonyabb fizetést a cégnél?
+
+            // 16. feladat: Mennyi Zoltán dolgozik a cégnél?
+
+            // 17. feladat: Van-e olyan dolgozója a cégnek, akinek két utóneve van?
+
+            // 18. feladat: Mennyi dolgozó csatlakozott a céghez 1980-ban?
+
+            // 19. feladat: Van-e gyártás részleg a cégnél?
+
+            // 20. feladat: Kik dolgoznak a legrégebben a cégnél?
+
+            // + feladat: Milyen részlegek vannak a cégnél?
+
+            // ++ feladat: Melyik részlegen a legmagasabb az átlag fizetés?
+
+            // +++ feladat: Listázd ki, hogy melyik évben mennyi dolgozó csatlakozott! A lista legyen év szerint rendezve!
+            // Azok az évek, amikor nem csatlakozott senki ne jelenjen meg!
+
+            Console.ReadKey();
+        }
+    }
+}
